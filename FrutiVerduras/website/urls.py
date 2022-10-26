8a6f03bbfcd535d279c63227bbf5bcf8a0d117e5
Victoria Vazquez <65380353+VickyA01654095@users.noreply.github.com>
2022-10-26 11:45:12 -0500

    varios cambios

--- FrutiVerduras/website/urls.py
+++ FrutiVerduras/website/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns=[
   path('', views.index),
   path('apartar', views.apartarProducto, name='apartar'),
+  path('productos', views.getProductos, name='productos'),
+  path('test', views.test, name='test'),
   
 ]
--- FrutiVerduras/website/views.py
+++ FrutiVerduras/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 from .models import Vendedor, Comprador, Producto, Producto_apartado, Mensaje, Chat
 from django.views.decorators.csrf import csrf_exempt
@@ -13,13 +13,55 @@ def index(request):
 
   return HttpResponse("Hello Geeks")
 
+def test(request):
+
+  return render(request, 'testShowApartados.html')
+
+
+def showApartados(request):
+  ## falta modificar 
+  mydb = sqlite3.connect("db.sqlite3")
+  curr = mydb.cursor()
+
+  productsQry = '''SELECT Producto.nombre, Producto.precio, Producto.estado_actual 
+  FROM Producto INNER JOIN Producto_apartado
+  ON Producto_apartado.producto_id = Producto.id WHERE Producto_apartado.comprador_id = 1'''
+
+  productsQry = curr.execute(productsQry)
+
+  list_products = []
+
+  for x in productsQry:
+    list_products.append([x[0], x[1], x[2]])
+  
+  mydb.commit()
+  mydb.close()
+
+  return render(request, 'index.html', {'apartados': list_products})
+
+def getProductos(request):
+  mydb = sqlite3.connect("db.sqlite3")
+  curr = mydb.cursor()
+
+  productsQry = '''SELECT nombre, precio, estado_actual, id FROM Producto'''
+  productsQry = curr.execute(productsQry)
+
+  list_products = []
+
+  for x in productsQry:
+    list_products.append([x[0], x[1], x[2], x[3]])
+  
+  mydb.commit()
+  mydb.close()
+
+  return render(request, 'index.html', {'products': list_products})
+
+
 @csrf_exempt
 def apartarProducto(request):
-  body_unicode = request.body.decode('utf-8')
-  body = loads(body_unicode)
-  user_id = body["user_id"]
-  product_id = body["prod_id"]
-  tiempo = body["time"]
+  user_id = 1
+  product_id = request.POST["prod_id"]
+  tiempo = "01:00"
 
   user = Comprador.objects.get(pk=user_id)
   prod = Producto.objects.get(pk=product_id)
@@ -31,4 +73,5 @@ def apartarProducto(request):
 
   json = dumps({"msg": "producto apartado"})
 
-  return HttpResponse(json, content_type="application/json")
+  return redirect('productos')
+
